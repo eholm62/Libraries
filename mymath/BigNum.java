913cf24601853d2ec1f7c6cb29ce6d2198d54888
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-04-01 15:03:56 +0000

    added Fraction and BigNum types

---
+++ mymath/BigNum.java
@@ -0,0 +1,29 @@
+package mymath;
+
+import java.math.BigInteger;
+
+public class BigNum
+{
+	public static final BigNum ONE = new BigNum(BigInteger.ONE, Fraction.ZERO);
+	
+	private final BigInteger integer;
+	private final Fraction fraction;
+
+	public BigNum(BigInteger integer, Fraction fraction)
+	{
+		this.integer = integer;
+		this.fraction = fraction;
+	}
+
+	public String toString()
+	{
+		return this.integer.toString() + " " + this.fraction.toString();
+	}
+
+	public BigNum add(BigNum other)
+	{
+		BigInteger integer = this.integer.add(other.integer);
+		Fraction fraction = this.fraction.add(other.fraction);
+		return new BigNum(integer, fraction);
+	}
+}
