f46731a54010d665798f3d16b57fc78a5264d35e
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-04-02 17:07:53 +0000

    finished all constructors and the toString method for mymath.BigNum

--- mymath/BigNum.java
+++ mymath/BigNum.java
@@ -1,36 +1,84 @@
 package mymath;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
 public class BigNum
 {
-	public static final BigNum ONE = new BigNum(BigInteger.ONE, Fraction.ZERO);
+	public static final 
 	
 	private final BigInteger integer;
-	private final Fraction fraction;
+	private final double fraction;
 
-	public BigNum(BigInteger integer, Fraction fraction)
+	// this is the most efficient 2-parameter constructor
+	/** Constructs a BigNum with integer part integer
+	 * and fractional part fraction.
+	 * Keep in mind fractional part  is always added.
+	 */
+	public BigNum(BigInteger integer, double fraction)
+	{
+		if (fraction < 0.0)
+		{
+			this.integer = integer.subtract(BigInteger.ONE);
+			this.fraction = 1.0 + fraction;
+		}
+		else
+		{
+			this.integer = integer;
+		    this.fraction = fraction;
+		}
+	}
+
+	// this is the most efficient 1-parameter constructor
+	/** Constructs a BigNum with integer part integer */
+	public BigNum(BigInteger integer)
 	{
 		this.integer = integer;
-		this.fraction = fraction;
+		this.fraction = 0.0;
 	}
+
+	/** Constructs a BigNum with integer part integer
+	 * and fractional part fraction.
+	 * Keep in mind fractional part is always added.
+	 */
+	public BigNum(long integer, double fraction)
+	{
+		if (fraction < 0.0)
+		{
+			this.integer = BigInteger.valueOf(integer).subtract(BigInteger.ONE);
+			this.fraction = 1.0 + fraction;
+		}
+		else
+		{
+			this.integer = BigInteger.valueOf(integer);
+		    this.fraction = fraction;
+		}
+	}
+
+	/** Constructs a BigNum with integer part integer */
+	public BigNum(long integer)
+	{
+		this.integer = BigInteger.valueOf(integer);
+		this.fraction = 0.0;
+	}
+
+	/** Constructs a BigNum with the value of value */
+	public BigNum(double value)
+	{
+		byte[] byteArray = new byte[8];
+		long lng = Double.doubleToLongBits(value);
+		for (int i = 0; i < 8; i++) 
+			byteArray[i] = (byte)((lng >> ((7 - i) * 8)) & 0xff);
+		
+		this.integer = new BigInteger(byteArray);
+		this.fraction = 0.0;
+	}	
 
 	public String toString()
 	{
-		return this.integer.toString() + " " + this.fraction.toString();
-	}
+		if (this.integer.compareTo(BigInteger.ZERO) == -1)
+			return this.integer.add(BigInteger.ONE).toString() + String.valueOf(1.0 - fraction).substring(1);
 
-	public BigNum add(BigNum other)
-	{
-		BigInteger integer = this.integer.add(other.integer);
-		Fraction fraction = this.fraction.add(other.fraction);
-		return new BigNum(integer, fraction);
-	}
-
-	public BigNum subtract(BigNum other)
-	{
-		BigInteger integer = this.integer.subtract(other.integer);
-		Fraction fraction = this.fraction.subtract(other.fraction);
-		return new BigNum(integer, fraction);
+		return this.integer.toString() + String.valueOf(this.fraction).substring(1);
 	}
 }
